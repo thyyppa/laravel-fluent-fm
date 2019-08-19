65f31cf6d5c1d83a4eb2c3eb4b321881d388050f
Travis Hyyppä <travishyyppa@gmail.com>
2019-08-19 13:51:58 -0600

    Add 'timeout' option and bump fluentfm version

--- config/fluent-fm.php
+++ config/fluent-fm.php
@@ -1,8 +1,9 @@
 <?php return [
 
-    'file' => env('FILEMAKER_FILE', 'FilemakerFilename'),
-    'host' => env('FILEMAKER_HOST', '127.0.0.1'),
-    'user' => env('FILEMAKER_USER', 'Admin'),
-    'pass' => env('FILEMAKER_PASS', 'secret'),
+    'file'    => env('FILEMAKER_FILE', 'FilemakerFilename'),
+    'host'    => env('FILEMAKER_HOST', '127.0.0.1'),
+    'user'    => env('FILEMAKER_USER', 'Admin'),
+    'pass'    => env('FILEMAKER_PASS', 'secret'),
+    'timeout' => env( 'FILEMAKER_TIMEOUT', 30 ),
 
 ];
