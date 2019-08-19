f76bd140318a0ef62f669075f76ba76914263ffe
Travis Hyyppä <travishyyppa@gmail.com>
2019-08-19 17:18:21 -0600

    Add config option to override client settings

--- config/fluent-fm.php
+++ config/fluent-fm.php
@@ -1,9 +1,17 @@
 <?php return [
 
-    'file'    => env('FILEMAKER_FILE', 'FilemakerFilename'),
-    'host'    => env('FILEMAKER_HOST', '127.0.0.1'),
-    'user'    => env('FILEMAKER_USER', 'Admin'),
-    'pass'    => env('FILEMAKER_PASS', 'secret'),
-    'timeout' => env( 'FILEMAKER_TIMEOUT', 30 ),
+    'file'    => env( 'FILEMAKER_FILE', 'FilemakerFilename' ),
+    'host'    => env( 'FILEMAKER_HOST', '127.0.0.1' ),
+    'user'    => env( 'FILEMAKER_USER', 'Admin' ),
+    'pass'    => env( 'FILEMAKER_PASS', 'secret' ),
+
+    'client' => [
+        'verify'              => false,
+        'http_errors'         => false,
+        'timeout'             => env( 'FILEMAKER_TIMEOUT', 30 ),
+        'headers'             => [ 'Connection' => 'close' ],
+        CURLOPT_FORBID_REUSE  => true,
+        CURLOPT_FRESH_CONNECT => true,
+    ],
 
 ];
